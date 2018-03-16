d1c1bf4e75f44e7efd5e5925e2dd3ca4af073953
Janne Saari <knutti24@gmail.com>
2018-03-16 09:31:55 +0200

    Renamed tablemodel and added second table.

---
+++ queuedclientstable.cpp
@@ -0,0 +1,6 @@
+#include "queuedclientstable.h"
+
+QueuedClientsTable::QueuedClientsTable()
+{
+
+}
---
+++ queuedclientstable.h
@@ -0,0 +1,11 @@
+#ifndef QUEUEDCLIENTSTABLE_H
+#define QUEUEDCLIENTSTABLE_H
+
+
+class QueuedClientsTable
+{
+public:
+    QueuedClientsTable();
+};
+
+#endif // QUEUEDCLIENTSTABLE_H
